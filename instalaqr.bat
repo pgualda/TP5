1be0e2d80a3dba849368f5ef55b6764d45a1e1b3
pgualda <111656559+pgualda@users.noreply.github.com>
2022-10-20 20:49:19 -0300

    Add files via upload

---
+++ instalaqr.bat
@@ -0,0 +1,7 @@
+echo off
+echo usamos la version de composer 2.2.10, funciona correctamente en windows 7, 64 bits
+
+echo qr-code-reader
+
+composer require libern/qr-code-reader
+
---
+++ instalaresize.bat
@@ -0,0 +1,6 @@
+echo off
+echo usamos la version de composer 2.2.10, funciona correctamente en windows 7, 64 bits
+
+echo php-image-resize
+
+composer require gumlet/php-image-resize
