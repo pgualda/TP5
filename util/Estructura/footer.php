8e658aadf6f220e5e98efc872059abf1c0c96976
pgualda <111656559+pgualda@users.noreply.github.com>
2022-10-20 21:19:32 -0300

    Add files via upload

---
+++ util/Estructura/footer.php
@@ -0,0 +1,27 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+<!--  <footer style="background-color: #eee6d3;"> -->
+  <footer style="bg-light">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="mb-3 text-dark">Librerias PHP</h5>
+          <p>
+          <a href="https://github.com/gumlet/php-image-resize" class="link-dark">gumlet/php-image-resize</a></br>
+          <a href="https://github.com/libern/qr-code-reader" class="link-dark">libern/qr-code-reader</a>
+          </p>
+        </div>
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="mb-3 text-dark">Integrantes</h5>
+          Norberto Gualda - Sabrina Gaspart
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      Tecnicatura Desarrollo Web - Programacion Web Dinámica - Grupo 4.1
+      <a class="text-dark" href="https://github.com/pgualda/TP5">Repositorio</a>
+    </div>
+  </footer>
+  
+</div>
---
+++ util/Estructura/header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Menu</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+
+  <link rel="stylesheet" href="../../util/bootstrap-5.2.0-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../util/css/estilos.css">
+</head>
+
+<body style="background-color: #efefef;">
+<header>
+    <!-- place navbar here -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <div class="container">
+            <a class="navbar-brand" href="../inicia/index.php">Librearias PHP</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../inicia/cargarimagen.php">Cargar imagen<br>para retamañar y escanear qr</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../inicia/listarimagen.php">Listado imagenes<br></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../inicia/listamodal.php">Listado imagenes <p class="text-info">(beta)</p><br></a>
+                    </li>
+                </ul>
+           </div>
+      </div>
+    </nav>
+</header>
+
+<script src="../util/bootstrap-5.2.0-dist/js/bootstrap.min.js"></script>
+
+<!--  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
+  </script> -->
+</body>
+
+</html>
